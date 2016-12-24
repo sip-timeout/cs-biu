87d71cf623ae8d24a54524e7d78693eb19c6e933
Oded <sip.timeout@gmail.com>
2016-12-24 16:53:31 +0200

    Part 1 - ass2 - NLP

---
+++ NLP/ass2/generate.py
@@ -0,0 +1,68 @@
+"""Generate.
+
+Usage:
+  generate.py <grammer> [-n <sentences>]
+
+Options:
+  -h --help     Show this screen.
+  -n <sentences>  number of sentences [default: 1]
+
+"""
+
+from collections import defaultdict
+import random
+from docopt import docopt
+class PCFG(object):
+    def __init__(self):
+        self._rules = defaultdict(list)
+        self._sums = defaultdict(float)
+
+    def add_rule(self, lhs, rhs, weight):
+        assert(isinstance(lhs, str))
+        assert(isinstance(rhs, list))
+        self._rules[lhs].append((rhs, weight))
+        self._sums[lhs] += weight
+
+    @classmethod
+    def from_file(cls, filename):
+        grammar = PCFG()
+        with open(filename) as fh:
+            for line in fh:
+                line = line.split("#")[0].strip()
+                if not line: continue
+                w,l,r = line.split(None, 2)
+                r = r.split()
+                w = float(w)
+                grammar.add_rule(l,r,w)
+        return grammar
+
+    def is_terminal(self, symbol): return symbol not in self._rules
+
+    def gen(self, symbol):
+        if self.is_terminal(symbol): return symbol
+        else:
+            expansion = self.random_expansion(symbol)
+            return " ".join(self.gen(s) for s in expansion)
+
+    def random_sent(self):
+        return self.gen("ROOT")
+
+    def random_expansion(self, symbol):
+        """
+        Generates a random RHS for symbol, in proportion to the weights.
+        """
+        p = random.random() * self._sums[symbol]
+        for r,w in self._rules[symbol]:
+            p = p - w
+            if p < 0: return r
+        return r
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    pcfg = PCFG.from_file(arguments['<grammer>'])
+
+    n = int(arguments['-n'])
+
+    for i in range(0,n):
+        print pcfg.random_sent()
