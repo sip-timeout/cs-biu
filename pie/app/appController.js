2a6634392b157846fb9d25c95f1d38a93d8cb3df
Oded <sip.timeout@gmail.com>
2017-04-07 16:11:08 +0300

    first commit - gui

---
+++ pie/app/appController.js
@@ -0,0 +1,42 @@
+(function () {
+
+    angular
+        .module('app')
+        .controller('AppController', [
+            'userService', '$location', '$mdSidenav', '$mdBottomSheet', '$timeout', '$log',
+            AppController
+        ]);
+
+
+    function AppController(userService, $location, $mdSidenav, $mdBottomSheet, $timeout, $log) {
+        var vm = this;
+
+
+        vm.stages = [
+            {
+                displayName: "Pick A Restaurant",
+                route: "/restaurant"
+            }
+        ]
+        vm.selected = vm.stages[0];
+        vm.toggleMenu = toggleMenu;
+        vm.selectStage = selectStage
+        vm.menuOpen = true
+
+        function toggleMenu() {
+            vm.menuOpen = !vm.menuOpen
+        }
+
+        /**
+         * Select the current avatars
+         * @param menuId
+         */
+        function selectStage(stage) {
+            vm.selected = stage
+            $location.path(stage.route)
+        }
+
+
+    }
+
+})();
