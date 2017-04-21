339def53a21737fed5a0d1da9058c0c8b90b0b19
Oded <sip.timeout@gmail.com>
2017-04-21 07:34:55 +0300

    removed unnecessary pages

--- multimedia/app/app.js
+++ multimedia/app/app.js
@@ -7,11 +7,7 @@
                 .defaultIconSet("./assets/svg/avatars.svg", 128)
                 .icon("menu", "./assets/svg/menu.svg", 24)
                 .icon("logo", "./assets/svg/logo.svg")
-                .icon("share", "./assets/svg/share.svg", 24)
-                .icon("google_plus", "./assets/svg/google_plus.svg", 512)
-                .icon("hangouts", "./assets/svg/hangouts.svg", 512)
-                .icon("twitter", "./assets/svg/twitter.svg", 512)
-                .icon("phone", "./assets/svg/phone.svg", 512);
+                .icon("share", "./assets/svg/share.svg", 24);
 
             $mdThemingProvider.theme('default')
 
