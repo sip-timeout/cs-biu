7b363305155c756e240a354d29bf1d6e914503e0
Oded <sip.timeout@gmail.com>
2017-12-16 18:57:17 +0200

    first draft quality extractor

--- yelp/YelpDB.py
+++ yelp/YelpDB.py
@@ -1,4 +1,6 @@
 import MySQLdb
+from MySQLdb.cursors import DictCursor
+from MySQLdb.cursors import Cursor
 
 
 class YelpDB:
@@ -34,7 +36,39 @@ class YelpDB:
                 select r.stars as review_rating,r.business_id as rest_id
                 from review r
                 where 'Restaurants' in ( select c.category from category c where c.business_id = r.business_id)
-                and r.user_id = {0}
+                and r.user_id = '{0}'
+                """
+            ,
+            "get_rest_info":
+                """
+                select name,city,state as country from business where id  = '{0}'
+                """
+            ,
+            "get_rest_categories":
+                """
+                select category from category where business_id = '{0}'
+                """
+            ,
+            "get_rest_attributes":
+                """
+                select name,value from attribute
+                    where business_id= '{0}'  
+                    and name in (
+                    'RestaurantsPriceRange2',
+                    'GoodForMeal',
+                    'RestaurantsGoodForGroups',
+                    'NoiseLevel',
+                    'RestaurantsAttire',
+                    'OutdoorSeating',
+                    'Ambience',
+                    'GoodForKids',
+                    'RestaurantsTableService',
+                    'Music',
+                    'HappyHour',
+                    'GoodForDancing',
+                    'RestaurantsCounterService',
+                    'DietaryRestrictions'
+                    )
                 """
         }
 
@@ -45,22 +79,27 @@ class YelpDB:
     def __exit__(self, exc_type, exc_value, traceback):
         self.conn.close()
 
-    def __perform_query__(self, name, *params):
+    def __perform_query__(self, name, cur_type, *params):
         query_str = self.procedures[name].format(*params)
-        cur = self.conn.cursor(MySQLdb.cursors.DictCursor)
+        cur = self.conn.cursor(cur_type)
         cur.execute(query_str)
         return cur.fetchall()
 
-    def get_rests(self,review_threshold,limit):
-        return self.__perform_query__("get_rests",review_threshold,limit)
+    def get_rests(self, review_threshold, limit):
+        return self.__perform_query__("get_rests", DictCursor, review_threshold, limit)
 
-    def get_rest_reviews(self,rest_id):
-        return self.__perform_query__("get_reviews",rest_id)
+    def get_rest_reviews(self, rest_id):
+        return self.__perform_query__("get_reviews", DictCursor, rest_id)
 
-    def get_users(self,limit):
-        return self.__perform_query__("get_users",limit)
+    def get_users(self, limit):
+        return self.__perform_query__("get_users", DictCursor, limit)
 
-    def get_user_reviews(self,user_id):
-        return self.__perform_query__("get_user_reviews",user_id)
+    def get_user_reviews(self, user_id):
+        return self.__perform_query__("get_user_reviews", DictCursor, user_id)
 
-    def get_rest_details(self,rest_id):
+    def get_rest_details(self, rest_id):
+        return {
+            'info': self.__perform_query__('get_rest_info', DictCursor, rest_id),
+            'category': self.__perform_query__('get_rest_categories', Cursor, rest_id),
+            'attributes': self.__perform_query__('get_rest_attributes', Cursor, rest_id)
+        }
--- yelp/YelpDataExtractor.py
+++ yelp/YelpDataExtractor.py
@@ -10,6 +10,8 @@ Options:
 
 """
 
+import itertools
+import ast
 from docopt import docopt
 from YelpDB import YelpDB
 
@@ -19,6 +21,26 @@ def get_restaurants_dict(user_reviews):
     for review in user_reviews:
         rest_dic[review['rest_id']] = {'rating': review['review_rating']}
     return rest_dic
+
+
+def get_rest_details(db, rest_id):
+    rest_db_obj = db.get_rest_details(rest_id)
+    rest = dict(rest_db_obj['info'][0])
+    rest['cuisine'] = list(itertools.chain.from_iterable(rest_db_obj['category']))
+    rest['cuisine'].remove('Restaurants')
+    rest['restaurant-features'] = list()
+    for name, val in rest_db_obj['attributes']:
+        if isinstance(val, int) and val > 0:
+            rest['restaurant-features'].append("_".join([name, val]))
+        elif val.startswith('{'):
+            val_as_dic = ast.literal_eval(val.replace('false','False').replace('true','True'))
+            for key in val_as_dic:
+                if val_as_dic[key]:
+                    rest['restaurant-features'].append("_".join([name, key]))
+        else:
+            rest['restaurant-features'].append("_".join([name, val]))
+
+    return rest
 
 
 def main():
@@ -35,8 +57,8 @@ def main():
                 user['restaurants'] = get_restaurants_dict(db.get_user_reviews(user['user_id']))
                 for rest in user['restaurants']:
                     if rest not in restaurants:
-                        restaurants[rest] =
-
+                        restaurants[rest] = get_rest_details(db, rest)
+                        print restaurants[rest]
 
 
 if __name__ == "__main__":
